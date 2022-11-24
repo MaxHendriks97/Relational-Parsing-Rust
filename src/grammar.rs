9f27256b6851674ef63ab58bbb387e244457f588
Max Hendriks <m.a.hendriks@student.utwente.nl>
2022-11-24 14:48:44 +0100

    Removed unnecessary "symbols" attribute

--- src/grammar.rs
+++ src/grammar.rs
@@ -6,7 +6,6 @@ use crate::finite_state_automaton::*;
 pub struct Grammar {
     pub terminals: HashSet<Terminal>,
     pub nonterminals: HashSet<Nonterminal>,
-    pub symbols: HashSet<Symbol>,
     pub start: Nonterminal,
     pub rules: HashMap<Nonterminal, HashSet<Word>>,
     pub finite_state_automaton: FiniteStateAutomaton,
@@ -14,14 +13,7 @@ pub struct Grammar {
 
 impl Grammar {
     pub fn new(terminals: HashSet<Terminal>, nonterminals: HashSet<Nonterminal>, start: Nonterminal, rules: HashMap<Nonterminal, HashSet<Word>>) -> Grammar {
-        let mut symbols: HashSet<Symbol> = HashSet::new();
-        for s in &nonterminals {
-            symbols.insert(Symbol::Nonterminal(*s));
-        }
-        for t in &terminals {
-            symbols.insert(Symbol::Terminal(*t));
-        }
         let finite_state_automaton = FiniteStateAutomaton::build_fsa(&terminals, start, &rules);
-        Grammar{terminals, nonterminals, symbols, start, rules, finite_state_automaton}
+        Grammar{terminals, nonterminals, start, rules, finite_state_automaton}
     }
 }
