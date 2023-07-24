f9166d821205cc21272fbaa2691d05c17c06423f
Max Hendriks <max@localhost.localdomain>
2023-07-24 09:58:57 +0200

    Rewrote e_rule test cases

--- tests/e_rule_relational_grammar_tests.rs
+++ tests/e_rule_relational_grammar_tests.rs
@@ -5,92 +5,95 @@ use relational_parsing::{
 
 mod common;
 
-fn get_inputs() -> ([Vec<Terminal>; 11], [Vec<Terminal>; 2]) {
-    ([
-        vec![],
-        vec!['a'],
-        vec!['a', 'a'],
-        vec!['a', 'a', 'a'],
-        vec!['a', 'b', 'a', 'c'],
-        vec!['a', 'b', 'a', 'c', 'a'],
-        vec!['a', 'b', 'a', 'a', 'c'],
-        vec!['a', 'b', 'a', 'b', 'a', 'c', 'c'],
-        vec!['a', 'a', 'b', 'a', 'b', 'a', 'c', 'c'],
-        vec!['b', 'b', 'b', 'b', 'c', 'c', 'c', 'c'],
-        vec!['b', 'c', 'b', 'c', 'b', 'c', 'a'],
-    ],
+fn get_test_cases() -> [(Vec<Terminal>, Result<RulesSet, ParseError>); 13] {
     [
-        vec!['a', 'a', 'a', 'b', 'a', 'c', 'c'],
-        vec!['a', 'a', 'b', 'a', 'b', 'a', 'c'],
-    ])
-}
-
-fn get_parses() -> [Result<RulesSet, ParseError>; 11] {
-    [
-        Ok(RulesSet::from_vec_slice(&[vec![('S', "e")]])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "a")]),
-           (vec![('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "Sa"), ('S', "a")]),
-           (vec![('S', "Sa"), ('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "Sa"), ('S', "Sa"), ('S', "a")]),
-           (vec![('S', "Sa"), ('S', "Sa"), ('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "SbSc"), ('S', "a"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "Sa"), ('S', "SbSc"), ('S', "a"), ('S', "a")]),
-           (vec![('S', "Sa"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
-           (vec![('S', "Sa"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "Sa"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "a"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "SbSc"), ('S', "Sa"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "a")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "a")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
-            (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")]),
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "a")]),
-           (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "e")]),
-        ])),
-        Ok(RulesSet::from_vec_slice(&[
-           (vec![('S', "Sa"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "e")]),
-        ]))
+        (vec![], Ok(RulesSet::from_vec_slice(&[vec![('S', "e")]]))),
+        (vec!['a'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "a")]),
+             (vec![('S', "Sa"), ('S', "e")])
+         ]))
+        ),
+        (vec!['a', 'a'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "Sa"), ('S', "a")]),
+             (vec![('S', "Sa"), ('S', "Sa"), ('S', "e")])
+         ])),
+        ),
+        (vec!['a', 'a', 'a'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "Sa"), ('S', "Sa"), ('S', "a")]),
+             (vec![('S', "Sa"), ('S', "Sa"), ('S', "Sa"), ('S', "e")])
+         ])),
+        ),
+        (vec!['a', 'b', 'a', 'c'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "SbSc"), ('S', "a"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
+         ])),
+        ),
+        (vec!['a', 'b', 'a', 'c', 'a'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "Sa"), ('S', "SbSc"), ('S', "a"), ('S', "a")]),
+             (vec![('S', "Sa"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
+             (vec![('S', "Sa"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "Sa"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
+         ])),
+        ),
+        (vec!['a', 'b', 'a', 'a', 'c'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "a"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "Sa"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")])
+         ])),
+        ),
+        (vec!['a', 'b', 'a', 'b', 'a', 'c', 'c'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e")]),
+         ])),
+        ),
+        (vec!['a', 'a', 'b', 'a', 'b', 'a', 'c', 'c'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "a"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "a"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "a"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "a")]),
+             (vec![('S', "SbSc"), ('S', "SbSc"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "e"), ('S', "Sa"), ('S', "Sa"), ('S', "e")]),
+         ])),
+        ),
+        (vec!['b', 'b', 'b', 'b', 'c', 'c', 'c', 'c'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "e")]),
+         ])),
+        ),
+        (vec!['b', 'c', 'b', 'c', 'b', 'c', 'a'],
+         Ok(RulesSet::from_vec_slice(&[
+             (vec![('S', "Sa"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "SbSc"), ('S', "e"), ('S', "e")]),
+         ]))
+        ),
+        (vec!['a', 'a', 'a', 'b', 'a', 'c', 'c'], Err(ParseError)),
+        (vec!['a', 'a', 'b', 'a', 'b', 'a', 'c'], Err(ParseError)),
     ]
 }
 
 #[test]
 fn e_rule_relational_grammar_recog_test() {
     let grammar = common::e_rule_relational_parsing_example_grammar();
-    let (accepted_inputs, rejected_inputs) = get_inputs();
+    let test_cases = get_test_cases();
+
 //     let mut memoize: Memoize = Memoize::new();
 
 //     assert!(relational_parsing::g_accepts_string(&vec![], &grammar, &mut memoize));
@@ -108,33 +111,19 @@ fn e_rule_relational_grammar_recog_test() {
 //     assert!(!relational_parsing::g_accepts_string(&vec!['a', 'a', 'a', 'b', 'a', 'c', 'c'], &grammar, &mut memoize));
 //     assert!(!relational_parsing::g_accepts_string(&vec!['a', 'a', 'b', 'a', 'b', 'a', 'c'], &grammar, &mut memoize));
 
-    for input in accepted_inputs.iter() {
-        assert!(relational_parsing::g_accepts_string_no_memo(input, &grammar));
-    }
-
-    for input in rejected_inputs.iter() {
-        assert!(!relational_parsing::g_accepts_string_no_memo(input, &grammar));
+    for (input, expected) in test_cases {
+        assert!(relational_parsing::g_accepts_string_no_memo(&input, &grammar) == expected.is_ok());
     }
 }
 
  #[test]
  fn e_rule_relational_grammar_parse_test() {
      let grammar = common::e_rule_relational_parsing_example_grammar();
-     let (accepted_inputs, rejected_inputs) = get_inputs();
-     let accepted_parses = get_parses();
-     for (input, parse) in accepted_inputs.iter().zip(accepted_parses) {
-         println!("input: {:?}", input);
-         let res = relational_parsing::find_parses_no_memo(input, &grammar);
-         println!("parse: {}", parse.as_ref().unwrap());
-         println!("res: {}", res.as_ref().unwrap());
-         assert_eq!(res, parse);
-     }
+     let test_cases = get_test_cases();
 
-     for input in rejected_inputs {
-         println!("input: {:?}", input);
-         assert!(relational_parsing::find_parses_no_memo(&input, &grammar).is_err());
+     for (input, expected) in test_cases {
+         assert_eq!(relational_parsing::find_parses_no_memo(&input, &grammar), expected);
      }
-
 //     let mut memoize: Memoize = Memoize::new();
 
 //     let tokens: Vec<char> = "babcbbabacbbacbbabbbabacbcbabababbbaababbbbabbabbabaaaaaacaccaaacaaaacacccacacacaaccaacacacaaaaaacccaaccaaaccccbacabacaaa".chars().collect();
